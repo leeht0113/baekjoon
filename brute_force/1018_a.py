16e13f420434c9a674132a64e2022c2b840d4551
leeht <leeht0113@gmail.com>
2023-10-15 16:59:18 +0900

    v9

---
+++ brute_force/1018_a.py
@@ -0,0 +1,25 @@
+# 체스판 다시 칠하기
+N, M = map(int, input().split())
+board = [input() for _ in range(N)]
+ans = 64
+
+def fill(y, x, bw):
+    global ans
+    cnt = 0
+    for i in range(8):
+        for j in range(8):
+            if (i + j) % 2:
+                if board[y + i][x + j] == bw:
+                    cnt += 1
+            else:
+                if board[y + i][x + j] != bw:
+                    cnt += 1
+
+    ans = min(ans, cnt)
+
+for i in range(N - 7):
+    for j in range(M - 7):
+        fill(i, j, 'B')
+        fill(i, j, 'W')
+
+print(ans)
